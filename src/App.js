67858da86f8e2361b6d8db5159e365f683629d6e
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-01-02 21:26:35 +0500

    Design the basic structure of BMI calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import React,{useState} from "react";
+import "./App.css";
 
-import './App.css';
-
-function App() {
-  <h1>Hello</h1>
-
-  
+function App() { 
+  const {weight,setWeight}= useState(0);
+  const {height,setHeight}= useState(0);
+  const {bmi,setBmi}= useState('');
+  const {message,setMessage} = useState('');
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>BMI Calculator</h2>
+        <form>
+          <div>
+            <label>Weight(lbs)</label>
+            <input
+              type="text"
+              placeholder="Enter Weight Value"
+              value={weight}
+            ></input>
+          </div>
+          <div>
+            <label>Height(lbs)</label>
+            <input
+              type="text"
+              placeholder="Enter height Value"
+              value={height}
+            ></input>
+          </div>
+          <div>
+            <button className="btn" type="submit">
+              Submit
+            </button>
+            <button
+              className="btn btn-outline"
+              // onClick={reload}
+              // type="submit"
+            
+            >Reload</button>
+          </div>
+          <div className="center">
+            <h3>Your BMI is:{bmi}</h3>
+            <p>{message}</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
